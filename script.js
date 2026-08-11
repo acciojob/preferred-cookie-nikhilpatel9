bd64c7eff813807c72bb3eb103a49bb2185a27f5
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-08-11 12:50:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const color = document.getElementById("fontcolor");
+const size = document.getElementById("fontsize");
+const save = document.getElementById("save");
+
+// Load saved preferences
+document.cookie.split("; ").forEach(cookie => {
+    const [name, value] = cookie.split("=");
+
+    if (name === "fontsize") {
+        size.value = parseInt(value);
+        document.documentElement.style.setProperty("--fontsize", value);
+    }
+
+    if (name === "fontcolor") {
+        color.value = value;
+        document.documentElement.style.setProperty("--fontcolor", value);
+    }
+});
+
+// Save preferences
+save.onclick = () => {
+    document.cookie = `fontsize=${size.value}px`;
+    document.cookie = `fontcolor=${color.value}`;
+
+    document.documentElement.style.setProperty("--fontsize", size.value + "px");
+    document.documentElement.style.setProperty("--fontcolor", color.value);
+};
